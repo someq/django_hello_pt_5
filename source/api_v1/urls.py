72ea78569da6df2023c0a088914879ac19f980b6
Farid <thesomeq@gmail.com>
2020-10-12 20:34:07 +0600

    Добавлено приложение API и пара точек входа.

--- source/api_v1/urls.py
+++ source/api_v1/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
+from api_v1.views import get_token_view, ArticleCreateView
+
 app_name = 'api_v1'
 
 urlpatterns = [
+    path('get-token/', get_token_view, name='get_token'),
+    path('article-create/', ArticleCreateView.as_view(), name='article_create')
 ]
--- source/api_v1/views.py
+++ source/api_v1/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+import json
+from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse
+from django.views.generic import View
+from django.views.decorators.csrf import ensure_csrf_cookie
 
-# Create your views here.
+from webapp.models import Article
+
+
+@ensure_csrf_cookie
+def get_token_view(request, *args, **kwargs):
+    if request.method == 'GET':
+        return HttpResponse()
+    return HttpResponseNotAllowed('Only GET request are allowed')
+
+
+class ArticleCreateView(View):
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        print(data)
+        article = Article.objects.create(
+            author_id=data['author_id'],
+            title=data['title'],
+            text=data['text']
+        )
+        return JsonResponse({
+            'pk': article.pk,
+            'author_id': article.author_id,
+            'title': article.title,
+            'text': article.text,
+            'created_at': article.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+            'updated_at': article.updated_at.strftime('%Y-%m-%d %H:%M:%S'),
+        })

92e08534ad8e1fa947ed08bfefa82f0b785d76ba
Farid <thesomeq@gmail.com>
2020-10-14 20:23:38 +0600

    Поправлен путь на создание статьи через API.

--- source/api_v1/urls.py
+++ source/api_v1/urls.py
@@ -6,5 +6,5 @@ app_name = 'api_v1'
 
 urlpatterns = [
     path('get-token/', get_token_view, name='get_token'),
-    path('article-create/', ArticleCreateView.as_view(), name='article_create')
+    path('article/create/', ArticleCreateView.as_view(), name='article_create')
 ]
